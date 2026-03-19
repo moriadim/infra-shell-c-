7e22e96b1f8035ee2e2a45d8c1525a096baac4fe
Moriaty-ctrl <a_merzougui@estin.dz>
2026-03-19 04:09:27 +0100

    refactor: move string parsing logic to parser.c

---
+++ parser.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+char **split_line(char *line) {
+    int bufsize = TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "infra-sh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens) {
+                fprintf(stderr, "infra-sh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
