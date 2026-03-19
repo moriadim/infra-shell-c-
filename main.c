101762df03c47230ba178cdde98f316fd3cb7d7a
Moriaty-ctrl <a_merzougui@estin.dz>
2026-03-19 04:00:36 +0100

    feat: add string tokenizer to handle command arguments

--- main.c
+++ main.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAX_LINE 1024
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " \t\r\n\a"
+
+char **split_line(char *line) {
+    int bufsize = TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char*));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char*));
+            if (!tokens) {
+                fprintf(stderr, "allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
 
 int main() {
     char line[MAX_LINE];
+    char **args;
 
     while (1) {
         printf("> ");
@@ -16,11 +50,23 @@ int main() {
 
         line[strcspn(line, "\n")] = 0;
 
+        // Ignore empty input
+        if (strlen(line) == 0) {
+            continue;
+        }
+
         if (strcmp(line, "exit") == 0) {
             break;
         }
 
-        printf("%s\n", line);
+        args = split_line(line);
+
+        // For demonstration, print parsed arguments
+        for (int i = 0; args[i] != NULL; i++) {
+            printf("arg[%d]: %s\n", i, args[i]);
+        }
+
+        free(args);
     }
 
     return 0;
