75fae919f42ab82fcbee8ce47cbb210d5cbfceae
Moriaty-ctrl <a_merzougui@estin.dz>
2026-03-19 04:04:17 +0100

    feat: add support for built-in cd command

--- main.c
+++ main.c
@@ -51,6 +51,17 @@ int execute_command(char **args) {
         return 0; // إشارة لإنهاء الحلقة
     }
 
+    if (strcmp(args[0], "cd") == 0) {
+        if (args[1] == NULL) {
+            fprintf(stderr, "shell expected argument to \"cd\"\n");
+        } else {
+            if (chdir(args[1]) != 0) {
+                perror("shell");
+            }
+        }
+        return 1;
+    }
+
     pid = fork();
     if (pid == 0) {
         // Child Process (العملية الابن)
